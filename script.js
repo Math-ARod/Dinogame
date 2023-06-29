d964870c5f63e4b787265a0f07f854886b406da1
Math-ARod <matheusvwg@gmail.com>
2023-06-29 15:26:36 -0300

    feat: final details

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let character = document.querySelector('#character')
 let square = document.querySelector('#square')
+let square2 = document.querySelector('#square2')
 
 function jump(){
     if(character.classList != 'anime'){
@@ -22,10 +23,22 @@ let testcollision = setInterval(function(){
         window.getComputedStyle(square).getPropertyValue('left')
     )
 
+    let leftSquare2 = parseInt(
+        window.getComputedStyle(square2).getPropertyValue('left')
+    )
+
     if(leftSquare < 45 && leftSquare > 0 && topCharacter >= 130){
         square.style.animation = 'none'
         square.style.display = 'none'
-        alert('SE FODEU!')
+        square2.style.animation = 'none'
+        square2.style.display = 'none'
+        alert('GAME OVER!')
+    }else if(leftSquare2 < 45 && leftSquare2 > 0 && topCharacter >= 130){
+        square2.style.animation = 'none'
+        square2.style.display = 'none'
+        square.style.animation = 'none'
+        square.style.display = 'none'
+        alert('GAME OVER!')
     }
 
 }, 10)
