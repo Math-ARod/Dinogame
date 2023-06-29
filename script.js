ae3e3004f6fbf56e4e1b11093d7807ad840991c5
Math-ARod <matheusvwg@gmail.com>
2023-06-29 14:13:45 -0300

    feat: implement hitbox

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ function jump(){
         character.classList.remove('anime')
     }, 500)
 }
+// o numero acima se refere a milisegundos
+
+let testcollision = setInterval(function(){
+
+    let topCharacter = parseInt(
+        window.getComputedStyle(character).getPropertyValue('top')
+    )
+
+    let leftSquare = parseInt(
+        window.getComputedStyle(square).getPropertyValue('left')
+    )
+
+    if(leftSquare < 45 && leftSquare > 0 && topCharacter >= 130){
+        square.style.animation = 'none'
+        square.style.display = 'none'
+        alert('SE FODEU!')
+    }
+
+}, 10)
