57329e4f8f8063e50964f0d93145283255607b2f
Math-ARod <matheusvwg@gmail.com>
2023-06-29 16:26:00 -0300

    feat: more final details

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let character = document.querySelector('#character')
 let square = document.querySelector('#square')
 let square2 = document.querySelector('#square2')
+let square3 = document.querySelector('#square3')
 
 function jump(){
     if(character.classList != 'anime'){
@@ -27,18 +28,42 @@ let testcollision = setInterval(function(){
         window.getComputedStyle(square2).getPropertyValue('left')
     )
 
+    let leftSquare3 = parseInt(
+        window.getComputedStyle(square3).getPropertyValue('left')
+    )
+
     if(leftSquare < 45 && leftSquare > 0 && topCharacter >= 130){
         square.style.animation = 'none'
         square.style.display = 'none'
         square2.style.animation = 'none'
         square2.style.display = 'none'
+        square3.style.animation = 'none'
+        square3.style.display = 'none'
         alert('GAME OVER!')
-    }else if(leftSquare2 < 45 && leftSquare2 > 0 && topCharacter >= 130){
+        location.reload()
+
+    }
+    
+    else if(leftSquare2 < 45 && leftSquare2 > 0 && topCharacter >= 130){
+        square2.style.animation = 'none'
+        square2.style.display = 'none'
+        square.style.animation = 'none'
+        square.style.display = 'none'
+        square3.style.animation = 'none'
+        square3.style.display = 'none'
+        alert('GAME OVER!')
+        location.reload()
+    }
+
+    else if(leftSquare3 < 45 && leftSquare3 > 0 && topCharacter >= 130){
+        square3.style.animation = 'none'
+        square3.style.display = 'none'
         square2.style.animation = 'none'
         square2.style.display = 'none'
         square.style.animation = 'none'
         square.style.display = 'none'
         alert('GAME OVER!')
+        location.reload()
     }
 
 }, 10)
